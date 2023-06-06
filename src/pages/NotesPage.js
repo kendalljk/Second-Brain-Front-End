01c80751a8489c095e32ac9b646c56e07029649c
kendalljk <120734396+kendalljk@users.noreply.github.com>
2023-06-06 08:03:44 -0700

    fixed paragraph rendering in notespage

--- src/pages/NotesPage.js
+++ src/pages/NotesPage.js
@@ -14,47 +14,61 @@ const NotesPage = ({noteCollection}) => {
         }));
     }
 
-
     return (
-    <Container className={`d-flex justify-content-center ${!noteCollection.length ? 'align-items-center' : ''}`}
-    style={{height: '100%'}}>
+    <Container className={`d-flex justify-content-center ${!noteCollection.length ? 'align-items-center' : ''
+    }`}
+    style={{height: '100%'}}
+    >
         <Row>
             <Col className='d-flex justify-content-center flex-wrap'>
             {noteCollection.length? (noteCollection.map((book) => (
-                <div className="flippable-card m-2" onClick={() => handleClick(book.key)}  key={book.key}>
+                <div 
+                className="flippable-card m-2" 
+                onClick={() => handleClick(book.key)}  
+                key={book.key}>
                     <Card 
                     className={`card-container ${flippedStates[book.key] ? 'flipped' : ''}`}
-                    style={{width: '22rem', height: '40rem'}}>
+                    style={{width: '22rem', height: '40rem'}}
+                    >
                         <div className='card-front'>
                             <Card.Body>
-                            <Card.Img 
-                            className='myNoteImg' 
-                            src={book.coverArtUrl} 
-                            alt={book.title}
-                            style={{width: '20rem', height: '32rem'}}/>
-                            <Card.Title>{book.title}</Card.Title>
-                            <Card.Text>
-                                <div>By: {book.author}</div>
-                            {book.finished? (
-                            <div className='py-2'>Finished: {book.dateFinished}</div>)
-                            : null}
-                            </Card.Text>
+                                <Card.Img 
+                                className='myNoteImg' 
+                                src={book.coverArtUrl} 
+                                alt={book.title}
+                                style={{width: '20rem', height: '32rem'}}
+                                />
+                                <Card.Title>{book.title}</Card.Title>
+                                <Card.Text>
+                                    <div>By: {book.author}</div>
+                                    {book.finished? (
+                                    <div className='py-2'>Finished: {book.dateFinished}</div>)
+                                    : null}
+                                </Card.Text>
                             </Card.Body>
                         </div>
                         <div className='card-back'>
-                            <Card.Body style={{ maxHeight: '39rem', overflowY: 'auto' }}>
+                            <Card.Body 
+                            style={{ maxHeight: '39rem', overflowY: 'auto' }}
+                            >
                                 <Card.Text>
                                     <div>
                                         <h6>Book Summary:</h6>
-                                        <p>{book.bookSummary}</p>
+                                        {book.bookSummary.split("\n\n").map((paragraph, index) => (
+                                        <p key={index}>{paragraph}</p>
+                                        ))}
                                     </div>
                                     <div>
                                         <h6>Favorite Quotes, Passages:</h6>
-                                        <p>{book.favoriteQuotes}</p>
+                                        {book.favoriteQuotes.split("\n\n").map((paragraph, index) => (
+                                        <p key={index}>{paragraph}</p>
+                                        ))}
                                     </div>
                                     <div>
                                         <h6>Favorite Quotes, Passages:</h6>
-                                        <p>{book.myThoughts}</p>
+                                        {book.myThoughts.split("\n\n").map((paragraph, index) => (
+                                        <p key={index}>{paragraph}</p>
+                                        ))}
                                     </div>
                                 </Card.Text>
                             </Card.Body>
