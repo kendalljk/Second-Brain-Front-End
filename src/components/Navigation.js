7192842b24d8fe585e3dd82a0f367af73586f199
kendalljk <120734396+kendalljk@users.noreply.github.com>
2023-06-15 15:47:25 -0700

    tabs styling, added modal to notespage

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,39 @@
 import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import logo from "../icons/logo.png";
+import { Offcanvas } from "react-bootstrap";
 
 export const Navigation = () => {
   return (
     <>
-      <Navbar bg="dark" variant="dark" expand="lg">
-        <Navbar.Brand as={Link} to="/">
+      <Navbar
+        bg="dark"
+        variant="dark"
+        expand="lg"
+        style={{
+          height: "8%",
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "flex-end",
+          paddingBottom: "0",
+        }}
+      >
+        <Navbar.Brand
+          as={Link}
+          to="/"
+          style={{
+            alignItems: "center",
+          }}
+        >
           <img src={logo} alt="brain logo" width="30" height="30" />
           Second Brain
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbar-nav" />
-        <Navbar.Collapse id="navbar-nav">
+        <Navbar.Toggle aria-controls="offCanvasDisplay" />
+        <Navbar.Offcanvas id="offCanvasDisplay" placement="end">
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title>Second Brain</Offcanvas.Title>
+          </Offcanvas.Header>
           <Nav justify="true" defaultActiveKey="/" variant="tabs">
             <Nav.Item>
               <Nav.Link as={Link} to="/" eventKey="/">
@@ -30,7 +51,7 @@ export const Navigation = () => {
               </Nav.Link>
             </Nav.Item>
           </Nav>
-        </Navbar.Collapse>
+        </Navbar.Offcanvas>
       </Navbar>
     </>
   );
