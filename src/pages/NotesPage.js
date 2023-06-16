b29808d899385a03b1f8485fb2422d5e7e6d4948
kendalljk <120734396+kendalljk@users.noreply.github.com>
2023-06-15 19:39:14 -0700

    Got rid of modals, back to the flip

--- src/pages/NotesPage.js
+++ src/pages/NotesPage.js
@@ -1,124 +1,119 @@
-import React, { useState, useEffect } from "react";
-import { Container, Card, Modal } from "react-bootstrap";
-
+import React from "react";
+import { Container, Col, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { useState } from "react";
 const NotesPage = ({ noteCollection }) => {
-  const [selectedBookKey, setSelectedBookKey] = useState(null);
-  const [selectedBook, setSelectedBook] = useState({});
-
-  useEffect(() => {
-    const book = noteCollection.find((note) => note.key === selectedBookKey);
-    setSelectedBook(book || {});
-  }, [selectedBookKey, noteCollection]);
-
-  const handleClose = () => {
-    setSelectedBookKey(null);
+  const [flippedStates, setFlippedStates] = useState({});
+  const handleClick = (key) => {
+    setFlippedStates((prevStates) => ({
+      ...prevStates,
+      [key]: !prevStates[key],
+    }));
   };
-
   return (
     <Container
-      onClick={(e) => {
-        let isClickInside = false;
-        document.querySelectorAll(".card-container").forEach((card) => {
-          if (card.contains(e.target)) {
-            isClickInside = true;
-          }
-        });
-
-        if (!isClickInside) {
-          handleClose();
-        }
-      }}
       className={`d-flex justify-content-center ${
         !noteCollection.length ? "align-items-center" : ""
       }`}
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        flexWrap: "wrap",
-        margin: "1%",
-      }}
+      style={{ height: "100%" }}
     >
-      {noteCollection.length ? (
-        noteCollection.map((book) => (
-          <div
-            className="card-container m-1"
-            onClick={() => {
-              setSelectedBookKey(book.key);
-            }}
-            key={book.key}
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              height: "20rem",
-              marginBottom: "1%",
-              position: "relative",
-            }}
-          >
-            <Card
-              style={{
-                width: "12rem",
-                height: "20rem",
-                position: "relative",
-              }}
-            >
-              <Card.Img
-                className="myNoteImg"
-                src={book.coverArtUrl}
-                alt={book.title}
-                style={{
-                  width: "100%",
-                  height: "13rem",
-                }}
-              />
-              <Card.Body
-                style={{
-                  padding: "2% 0",
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
+      <Row>
+        <Col className="d-flex justify-content-center flex-wrap">
+          {noteCollection.length ? (
+            noteCollection.map((book) => (
+              <div
+                className="flippable-card m-2"
+                onClick={() => handleClick(book.key)}
+                key={book.key}
               >
-                <Card.Title as="h6">{book.title}</Card.Title>
-                <Card.Text>
-                  <div>By: {book.author}</div>
-                  {book.finished ? (
-                    <div className="py-2">Finished: {book.dateFinished}</div>
-                  ) : null}
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </div>
-        ))
-      ) : (
-        <Container>
-          <h2>There are no notes to display yet - get reading!</h2>
-        </Container>
-      )}
-
-      <Modal show={!!selectedBookKey} onHide={handleClose} centered>
-        <Modal.Header closeButton>
-          <Modal.Title>{selectedBook.title}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <h6>Book Summary:</h6>
-          {selectedBook.bookSummary?.split("\n\n").map((paragraph, index) => (
-            <p key={index}>{paragraph}</p>
-          ))}
-          <h6>Favorite Quotes, Passages:</h6>
-          {selectedBook.favoriteQuotes
-            ?.split("\n\n")
-            .map((paragraph, index) => (
-              <p key={index}>{paragraph}</p>
-            ))}
-          <h6>My Thoughts:</h6>
-          {selectedBook.myThoughts?.split("\n\n").map((paragraph, index) => (
-            <p key={index}>{paragraph}</p>
-          ))}
-        </Modal.Body>
-      </Modal>
+                <Card
+                  className={`card-container ${
+                    flippedStates[book.key] ? "flipped" : ""
+                  }`}
+                  style={{
+                    width: flippedStates[book.key] ? "23rem" : "12rem",
+                    height: flippedStates[book.key] ? "40rem" : "20rem",
+                  }}
+                >
+                  <div className="card-front">
+                    <Card.Img
+                      className="myNoteImg"
+                      src={book.coverArtUrl}
+                      alt={book.title}
+                      style={{
+                        width: "100%",
+                        height: "13rem",
+                      }}
+                    />
+                    <Card.Body
+                      style={{
+                        padding: "2% 0",
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "center",
+                      }}
+                    >
+                      <Card.Title as="h6">{book.title}</Card.Title>
+                      <Card.Text>
+                        <div>By: {book.author}</div>
+                        {book.finished ? (
+                          <div className="py-2">
+                            Finished: {book.dateFinished}
+                          </div>
+                        ) : null}
+                      </Card.Text>
+                    </Card.Body>
+                  </div>
+                  <div className="card-back">
+                    <Card.Body
+                      style={{
+                        height: "39rem",
+                        width: "22rem",
+                        overflowY: "auto",
+                      }}
+                    >
+                      <Card.Text>
+                        <div>
+                          <h6>Book Summary:</h6>
+                          {book.bookSummary
+                            .split("\n\n")
+                            .map((paragraph, index) => (
+                              <p key={index}>{paragraph}</p>
+                            ))}
+                        </div>
+                        <div>
+                          <h6>Favorite Quotes, Passages:</h6>
+                          {book.favoriteQuotes
+                            .split("\n\n")
+                            .map((paragraph, index) => (
+                              <p key={index}>{paragraph}</p>
+                            ))}
+                        </div>
+                        <div>
+                          <h6>Favorite Quotes, Passages:</h6>
+                          {book.myThoughts
+                            .split("\n\n")
+                            .map((paragraph, index) => (
+                              <p key={index}>{paragraph}</p>
+                            ))}
+                        </div>
+                      </Card.Text>
+                    </Card.Body>
+                  </div>
+                </Card>
+              </div>
+            ))
+          ) : (
+            <Container>
+              <Row>
+                <h2>There are no notes to display yet- get reading!</h2>
+              </Row>
+            </Container>
+          )}
+        </Col>
+      </Row>
     </Container>
   );
 };
-
 export default NotesPage;
